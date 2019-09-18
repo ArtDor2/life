8d9df9953993a8793b5341d4b595f20e9a89995f
Artur Dorovskikh <19955193+ArtDor2@users.noreply.github.com>
2019-09-18 13:47:07 -0500

    Add files via upload

--- gameOfLife.py
+++ gameOfLife.py
@@ -1,23 +1,26 @@
-# Artur Dorovskikh 2017 Jan 27-28, Feb 4
-# My first medium size project in python and in programming
+# Artur Dorovskikh 2019-09-18 updated
+# Conway's game of life
 
-import sys, pygame as pg, random
+import sys
+import pygame as pg
+import random
+
 pg.init()
 
 # Set window resolution
-screen_w = 600
-screen_h = 600
+screen_w = screen_h = 400
 screen = pg.display.set_mode((screen_w,screen_h))
 
 # Set the colors
 c_white = (255, 255, 255)
 c_black = (0, 0, 0)
+c_green = (0, 128, 0)
+c_red = (255, 0, 0)
+c_yellow = (255, 255, 0)
 
 # Set cells and array
-cell_s = 1
-cell_s_h = int(cell_s/2)
-cells_x_l = int(screen_w/cell_s)  # Used in for loops, because a list starts with 0 then 1
-cells_y_l = int(screen_h/cell_s)  # Used in for loops, because a list starts with 0 then 1
+cells_x_l = int(screen_w)  # Used in for loops, because a list starts with 0 then 1
+cells_y_l = int(screen_h)  # Used in for loops, because a list starts with 0 then 1
 cells_x = cells_x_l - 1  # Used for checking neighbors
 cells_y = cells_y_l - 1  # Used for checking neighbors
 cells = [[0 for x in range(cells_x_l)] for y in range(cells_y_l)]
@@ -29,62 +32,55 @@ for x in range(cells_x_l):
         if random.randint(0, 19) == 1:  # Randomly fill in cells
             cells[x][y] = 1
 
-screen.fill(c_white)
+screen.fill(c_black)
 pg.display.update()
 
 
 # Count cell nearest neighbors
 def count_cells(): # x_cell, y_cell, radius):
-    value = 0
+    return cells[x][(y + 1) % cells_x] + \
+        cells[(x + 1) % cells_x][y] + \
+        cells[(x + 1) % cells_x][(y + 1) % cells_y] + \
+        cells[x][y - 1] + \
+        cells[x - 1][y] + \
+        cells[x - 1][y - 1] + \
+        cells[x - 1][(y + 1) % cells_y] + \
+        cells[(x + 1) % cells_x][y - 1]
 
-    if cells[x][(y + 1) % cells_x] == 1:  # Down
-        value += 1
-    if cells[(x + 1) % cells_x][y] == 1: # Right
-        value += 1
-    if cells[(x + 1) % cells_x][(y + 1) % cells_y] == 1: # Down right
-        value += 1
 
-    if cells[x][y - 1] == 1:  # Up
-        value += 1
-    if cells[x - 1][y] == 1:  # Left
-        value += 1
-    if cells[x - 1][y - 1] == 1:  # Up Left
-        value += 1
+def draw_pixel(pixel_color, pixel_x, pixel_y):
+    pg.draw.line(screen, pixel_color, (pixel_x,pixel_y), (pixel_x,pixel_y)) # 4.3 fps
+    #pg.draw.circle(screen, pixel_color, (pixel_x,pixel_y), 1) # 4fps
+    #return # drawing nothing is 4 fps
 
-    if cells[x - 1][(y + 1) % cells_y] == 1:  # Down Left
-        value += 1
-    if cells[(x + 1) % cells_x][y - 1] == 1:  # Up Right
-        value += 1
-
-    return value
+clock = pg.time.Clock() # to calculate fps
 
 while True:
+    screen.fill(c_black)
     for x in range(cells_x_l):
         for y in range(cells_y_l):
 
-            if count_cells() == 3:
-                cells_new[x][y] = 1
-            elif count_cells() == 2 and cells[x][y] == 1:
-                cells_new[x][y] = 1
-            else:
-                cells_new[x][y] = 0
+            cell_alive = cells[x][y]
+            cell_count = count_cells()
 
-            if step == (99 or 199 or 299):
-                if cells[x][y] == 1 and cells_new[x][y] == 1:
-                        pg.draw.rect(screen, c_black, (x * cell_s - cell_s_h, y * cell_s - cell_s_h, cell_s, cell_s), 0)
-                elif cells[x][y] == 0 and cells_new[x][y] == 0:
-                    pg.draw.rect(screen, c_white, (x * cell_s - cell_s_h, y * cell_s - cell_s_h, cell_s, cell_s), 0)
+            if cell_alive == 1:
+                if cell_count < 2 or cell_count > 3:
+                    cells_new[x][y] = 0
+                    draw_pixel(c_red, x, y)
+            else:
+                if cell_count == 3:
+                    cells_new[x][y] = 1
+                    draw_pixel(c_green, x, y)
+
+            if cell_alive and cells_new[x][y] == 1:
+                draw_pixel(c_white, x, y)
 
     cells, cells_new = cells_new, cells
     step += 1
 
-    pg.display.set_caption(str(step))
-    if step == (100 or 200 or 300):
-        pg.display.update()
-    # pg.time.wait(10)
-    # print(str(x) + ' ' + str(y))
-    # pg.time.wait(5000)
-    # pg.display.update()
+    clock.tick()
+    pg.display.set_caption(str(clock.get_fps())) # str(step) + 
+    pg.display.update()
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
